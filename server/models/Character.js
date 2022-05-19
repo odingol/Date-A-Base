45e78c59fd86b1296b6b4683b5348a1e32d8d3fd
Taylor25et <taylor25et@gmail.com>
2022-05-18 18:24:14 -0700

    Added starter files for project

---
+++ server/models/Character.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+const characterSchema = new Schema({
+  avatar: {
+      type: String,
+    },
+  
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+const Character = model('Character', characterSchema);
+
+module.exports = Character;
