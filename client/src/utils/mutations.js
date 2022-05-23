c50597f039461bc23b641a4417a0f0a16212df40
NathanM <nathan.milburn@outlook.com>
2022-05-22 17:16:00 -0700

    added components folder and utils folder with boilerplate files that may need changing

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client';
+
+export const ADD_PROFILE = gql`
+  mutation addProfile($name: String!, $email: String!, $password: String!) {
+    addProfile(name: $name, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const ADD_CHARACTER = gql`
+  mutation addCharacter($profileId: ID!, $character: String!) {
+    addCharacter(profileId: $profileId, character: $character) {
+      _id
+      name
+      characters
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
