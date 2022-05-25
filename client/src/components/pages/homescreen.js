a4a0eadb1d15b6586ad3a539d73d9526fe243441
Taylor25et <taylor25et@gmail.com>
2022-05-24 21:13:01 -0700

    getting site to render

--- client/src/components/pages/homescreen.js
+++ client/src/components/pages/homescreen.js
@@ -4,23 +4,35 @@
 
 // Insert username for characters to reference in their dialogue
 
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Login from "./Login";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 const Home = () => {
-
-    return (
-        <div className="container homepage">
-            <div className="userInfo">
-
-            </div>
-            <div className="playerMatches">
-
-            </div>
-            <button className="startGameButton">Start Your Dating Adventure!</button>
-        </div>
-    )
-}
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg" className="container homepage">
+        <Box>
+          <Container maxWidth="md" className="userInfo">
+            <img></img>
+            <p>USER INFO GOES HERE</p>
+          </Container>
+          <Container maxWidth="sm" className="matchHistory">
+            <button className="startGameButton">
+              <Link className="startGame" to="/gamepage">
+              Start Your Dating Adventure!
+              </Link>
+            </button>
+          </Container>
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default Home;
