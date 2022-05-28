f7cd8693d337b66f99de4aa5dedab133ddc609bc
Lamor Odingo <l2odingo@gmail.com>
2022-05-28 13:04:11 -0700

    styled game page

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,9 +1,39 @@
 import React, { useState } from "react";
-import { Link, Typography } from "@mui/material";
-import {QUERY_CHARACTERS} from "../utils/queries";
-import {useQuery} from '@apollo/client';
+// import { Link } from "@mui/material";
+import { QUERY_CHARACTERS } from "../utils/queries";
+import { useQuery } from '@apollo/client';
+import {
+  Grid,
+  Paper,
+  // Select,
+  Button,
+  // MenuItem,
+  // TextField,
+  Container,
+  Typography,
+  // InputLabel,
+  // FormControl,]
+  Link
+} from "@mui/material";
+
+import { makeStyles } from '@material-ui/core/styles';
+// import { ClassNames } from "@emotion/react";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage:`url(${process.env.PUBLIC_URL + '/assets/backdrop2.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+}));
 
 export default function Game(props) {
+  const classes = useStyles();
   
   const { loading, data } = useQuery(QUERY_CHARACTERS);
 
@@ -20,7 +50,9 @@ export default function Game(props) {
     return <div>Loading...</div>;
   }
 
+
   return (
+  <div className={classes.root}>
     <Typography
     variant="body2"
     color="text.secondary"
@@ -32,7 +64,100 @@ export default function Game(props) {
     <p key= {characterPrompt.id} >Hello my name is {characterPrompt.name} here is my prompt: {characterPrompt.prompt}</p>
     ))}
     </div> */}
-    
+    <Container>
+      <Paper style={{padding: 80, border: "3px solid black", width: '50rem', backgroundColor: 'hsla(0, 100%, 90%, 0.8'}}>
+          <>
+            <Typography variant="h1" style={{padding: 20}}>
+              Images
+            </Typography>
+            <Paper style={{border: "1px solid black", width: "50rem", height: "5rem"}} sx={{mb:3}}>
+              <h1>Dialogue</h1>
+            </Paper>
+                {/* <Grid item xs={12}> */}
+                  {/* <FormControl fullWidth variant="outlined">
+                    <InputLabel id="category-select-label">
+                      Select category:
+                    </InputLabel>
+                    <Select
+                      required
+                      name="category"
+                      id="category-select"
+                      label="Select category"
+                      labelId="category-select-label"
+                    >
+                        <MenuItem>
+                        Hello
+                        </MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl fullWidth variant="outlined">
+                    <InputLabel id="difficulty-select-label">
+                      Select Difficulty:
+                    </InputLabel>
+                    <Select
+                      required
+                      name="difficulty"
+                      id="difficulty-select"
+                      label="Select Difficulty"
+                      labelId="difficulty-select-label"
+                    >
+                        <MenuItem>
+                          Good Afternoon
+                        </MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    inputProps={{ min: 1, max: 10 }}
+                    required
+                    fullWidth
+                    type="number"
+                    id="quiz-number"
+                    variant="outlined"
+                    name="quiz-number"
+                    label={`Add a quiz number from 1 to 10`}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    inputProps={{ min: 1, max: 10 }}
+                    required
+                    fullWidth
+                    type="number"
+                    id="quiz-number"
+                    variant="outlined"
+                    name="quiz-number"
+                    label={`Add a quiz number from 1 to 10`}
+                  />
+                </Grid> */}
+                <Grid container spacing ={1} columns={16}>
+                  <Grid item xs={16}>
+               <Button variant="outlined" style={{width: '40rem', padding: 15 }}>Prompt</Button>
+               </Grid>
+               <Grid item xs={16}>
+               <Button variant="outlined" style={{width: '40rem', padding: 15}}>Prompt</Button>
+               </Grid>
+               <Grid item xs={16}>
+               <Button variant="outlined" style={{width: '40rem', padding: 15 }}>Prompt</Button>
+               </Grid>
+               <Grid item xs={16}>
+               <Button variant="outlined" style={{width: '40rem', padding: 15}}>Prompt</Button>
+               </Grid>
+               </Grid>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                sx={{m:3}}
+              >
+                Submit
+              </Button>
+          </>
+      </Paper>
+    </Container>
       {"Copyright © "}{" "}
       <Link color="inherit" href="https://mui.com/">
         DataBase
@@ -40,5 +165,6 @@ export default function Game(props) {
       {new Date().getFullYear()}
       {"."}
     </Typography>
+  </div>
   );
 }
