6732a257d3b0a562ea1adb1586191aad9340310d
NathanM <nathan.milburn@outlook.com>
2022-05-28 12:46:33 -0700

    added readme template and descriptions for each character.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,27 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { Link, resolvePath } from "react-router-dom";
 // import { useQuery } from '@apollo/client';
 // import { QUERY_CHARACTERS } from "../utils/queries";
 // Styling Imports
 import { CssBaseline, Box, Container } from "@mui/material";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Paper, Typography, Button } from "@mui/material";
+import { Paper, Typography, Button, Grid, withStyles } from "@mui/material";
 import homeImage from "../assets/images/UWbackdrop.png";
 
 const homeStyle = {
   paperContainer: {
     backgroundImage: `url(${homeImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
   },
 };
+
+const WhiteTextTypography = withStyles({
+  root: {
+    color: "#FFFFFF"
+  }
+})(Typography);
 
 export default function Home(props) {
   // const { loading, data } = useQuery(QUERY_CHARACTERS);
@@ -29,31 +37,49 @@ export default function Home(props) {
             <Box>
               <Container maxWidth="md" className="userInfo">
                 {/* <img></img>  */}
-                <h3>Welcome to Date-A-Base!</h3>
+                <h3 align="center">Welcome to Date-A-Base!</h3>
                 <p>
-                  Data-A-Base is a dating simulator experience that will pull your heartstrings as your start your journey as a full-stack web developer. 
+                  Data-A-Base is a dating simulator experience that will pull
+                  your heartstrings and build new connections as you start your
+                  journey to becoming a full-stack web developer.
                 </p>
                 <p>
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                  occaecat cupidatat non proident, sunt in culpa qui officia
-                  deserunt mollit anim id est laborum.
+                  Here you will meet fellow classmates looking to start study
+                  groups for the upcoming course. It will be up to you to decide
+                  who you will pair with for a chance at good grades and maybe
+                  even a chance at love?
                 </p>
-                <p>
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                  occaecat cupidatat non proident, sunt in culpa qui officia
-                  deserunt mollit anim id est laborum.
-                </p>
-                <p>
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                  occaecat cupidatat non proident, sunt in culpa qui officia
-                  deserunt mollit anim id est laborum.
-                </p>
+                <h3>Your Dating Profile:</h3>
+                <ul>
+                  <li>Name:</li>
+                  <li>Gender: Female</li>
+                  <li>
+                    Description: A go-getter student looking to take on the
+                    challenge of full-stack web development and begin their
+                    coding career journey.
+                  </li>
+                </ul>
+              </Container>
+              <Container maxWidth="md">
+                <h3>Your Teacher</h3>
+                <ul>
+                  <li>Name: Brian</li>
+                  <li>Gender: Male</li>
+                  <li>
+                    Description: A really cool guy, who cares for his student's
+                    growth and impeccable coding skills. Likes getting haircuts
+                    regularly, is a dad, and went to Michigan State University.
+                  </li>
+                </ul>
+                {/* Image of Brian */}
+              </Container>
+              <Container maxWidth="md">
+                <h3>Your Classmates</h3>
+                {/* Images of all the candidates */}
               </Container>
               {/* Saved Character Section */}
               <Container maxWidth="sm" className="matchHistory">
+                <h3>Your Matches</h3>
                 {/* <div className="col-12 col-md-10 my-3">
               {loading ? (
                 <div>Loading...</div>
@@ -72,13 +98,20 @@ export default function Home(props) {
                   deserunt mollit anim id est laborum.
                 </p>
               </Container>
-              <Container maxWidth="sm" className="matchHistory">
-                <Button variant="contained" align="center" className="startGameButton">
+              <div>
+                <Grid container justify="center">
+                <Button
+                  variant="contained"
+                  align="right"
+                  className="startGameButton"
+                  size="large"
+                >
                   <Link className="startGame" to="/game">
-                    Start Your Dating Adventure!
+                    JOIN CLASS
                   </Link>
                 </Button>
-              </Container>
+                </Grid>
+                </div>
             </Box>
           </Container>
         </Paper>
